356d8fc61d72c9b7e464d88185efc5924f512c8e
lcoelho3412 <leandrocoelho3@yahoo.com>
2023-11-06 15:06:35 -0300

    data types

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,13 @@
 public class HelloWorld {
+ public static void main(String[] args) {
+  //common types of data
 
+  int intData = 10;
+  double doubleData = 3.14; 
+  float floatData = 3.14f;
+  long longData = 3001245122L;
+  String stringData = "in double quotes only";
+  boolean booleData = false;
+
+ }
 }
